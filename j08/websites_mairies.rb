5392ab3ebc12899ec03971152d43aca64d7d37aa
Antonin <antonin.siguierfr@gmail.com>
2018-01-18 17:12:24 +0100

    Exo almost done

--- j08/websites_mairies.rb
+++ j08/websites_mairies.rb
@@ -9,9 +9,9 @@ def get_the_email_of_a_townhal_from_its_webpage
 	page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/vaureal.html"))
 
 	links = page.css("tr td p font")
-	links.each do |link|
-		if link.text.include?("@")
-			puts link.text
+	links.each do |elem|
+		if elem.text.include?("@")
+			puts elem.text
 		end
 	end
 # binding.pry
@@ -19,14 +19,17 @@ end
 
 def get_all_the_urls_of_val_doise_townhalls
 	page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+	website = Hash.new
+	my_arr = []
 
 	links = page.css(".lientxt")
-	links.each do |link|
-		clean_link = link['href'][1..-1] 
-		puts "annuaire-des-mairies.com" + clean_link
+	links.each do |elem|
+		#puts elem.text
+		clean_link = "annuaire-des-mairies.com" + elem['href'][1..-1] 
+		website[elem.text] = clean_link
 	end
+	puts website # Uncomment ca pour display le Hash
 end
-
 get_all_the_urls_of_val_doise_townhalls
 
 # get_the_email_of_a_townhal_from_its_webpage
