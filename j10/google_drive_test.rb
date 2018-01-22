3ec536d6f9c6429285a8ab025e430fa3ed614657
Antonin <antonin.siguierfr@gmail.com>
2018-01-22 13:51:37 +0100

    Test google drive

---
+++ j10/google_drive_test.rb
@@ -0,0 +1,19 @@
+require "google_drive"
+
+# Creates a session. This will prompt the credential via command line for the
+# first time and save it to config.json file for later usages.
+# See this document to learn how to create config.json:
+# https://github.com/gimite/google-drive-ruby/blob/master/doc/authorization.md
+session = GoogleDrive::Session.from_config("config.json")
+
+# Gets list of remote files.
+session.files.each do |file|
+  p file.title
+end
+
+# Telecharge un fichier du google drive et l'enregistre en local
+file = session.file_by_title("hello.txt") 
+file.download_to_file("/Users/antoninsiguier/Desktop/hello.txt")
+
+# Ajoute fichier en local au google drive
+# session.upload_from_file("/Users/antoninsiguier/Desktop/hello.txt", "hello.txt", convert: false)
