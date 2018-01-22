86dd8e216555897011d43b6fe40f162eb5a39a54
Antonin <antonin.siguierfr@gmail.com>
2018-01-22 15:48:10 +0100

    scrapping spreadsheet working

--- j10/exo_spreadsheet.rb
+++ j10/exo_spreadsheet.rb
@@ -33,9 +33,15 @@ get_all_the_urls_of_val_doise_townhalls(PAGE_URL)
 session = GoogleDrive::Session.from_config("config.json")
 @ws = session.spreadsheet_by_key("1kLN7UPXNdg4HJqRk_R5h0YOnGoo-wBmahCQ0Kx8qZ4g").worksheets[0]
 i = 1
+j = 1
 
 while i <= @table.length
-    @ws[i,1] = @table[i-1]["city_name"] && @ws[i,2] = @table[i-1]["city_email"]
+    @ws[i,1] = @table[i-1]["city_name"] 
     i += 1
+end
+
+while j <= @table.length
+    @ws[j,2] = @table[j-1]["city_email"]
+    j += 1
 end
 @ws.save
